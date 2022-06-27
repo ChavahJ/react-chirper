cc1c6a0b3ab7a305a05f177a4522e385ad15936e
Chavah Jacobs <chavah.jacobs@gmail.com>
2022-06-27 19:28:07 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,42 @@
-const App = () => {
-  return <div>Starter Code</div>;
+import { useEffect, Fragment } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
+import LoadingBar from "react-redux-loading-bar";
+import NewTweet from "./NewTweet";
+import TweetPage from "./TweetPage";
+import Nav from "./Nav";
+import { Routes, Route } from "react-router-dom";
+
+// The goal is, when the component mounts, we want to dispatch the invocation of our handleInitialData action creator
+// In order to get access to dispatch, we first have to connect our App component
+
+const App = (props) => {
+  useEffect(() => {
+    props.dispatch(handleInitialData());
+  }, []);
+  return (
+    <Fragment>
+      <LoadingBar />
+      <div className="container">
+        <Nav />
+        {props.loading === true ? null : (
+          <Routes>
+            <Route path="/" exact element={<Dashboard />} />
+            <Route path="/tweet/:id" element={<TweetPage />} />
+            <Route path="/new" element={<NewTweet />} />
+          </Routes>
+        )}
+      </div>
+    </Fragment>
+  );
 };
 
-export default App;
+const mapStateToProps = ({ authedUser }) => ({
+  loading: authedUser === null,
+});
+
+export default connect(mapStateToProps)(App);
+
+// now the App component has access to dispatch via Props
+// pass props into App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import middleware from "./middleware";
 import "./index.css";
 import App from "./components/App";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, middleware);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
